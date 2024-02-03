632d83e6f7898edf968c0da4adfd768afd8eb6a1
Dominique Louis <savagesoftware@gmail.com>
2024-02-03 15:30:40 +0000

    Initial checkin from dotnet new resharper-rider-plugin --name MeadowPlugin

---
+++ src/dotnet/ReSharperPlugin.MeadowPlugin.Tests/TestEnvironment.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace RiderPlugin.MeadowPlugin.Tests
+{
+    [ZoneDefinition]
+    public class MeadowPluginTestEnvironmentZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>, IRequire<IMeadowPluginZone> { }
+
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<MeadowPluginTestEnvironmentZone> { }
+
+    [SetUpFixture]
+    public class MeadowPluginTestsAssembly : ExtensionTestEnvironmentAssembly<MeadowPluginTestEnvironmentZone> { }
+}
